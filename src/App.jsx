15afa5ef99ffd558a7cbfb8070157ad712aadcab
chilsurr <suryaadevs17@gmail.com>
2025-10-26 02:18:26 +0700

    nambahin spline3d di contact

--- src/App.jsx
+++ src/App.jsx
@@ -19,6 +19,7 @@ function App() {
         <Sklills/>
         <Contact/>
         
+      
       </div>
     </>
   )
--- src/componen/skills.jsx
+++ src/componen/skills.jsx
@@ -1,4 +1,25 @@
+import VanillaTilt from "vanilla-tilt";
+import { useEffect } from "react";
 function Sklills() {
+
+  useEffect(() => {
+    // Pilih semua elemen dengan class 'skill-card'
+    const cards = document.querySelectorAll(".skill-card");
+
+    // Inisialisasi Vanilla Tilt
+    VanillaTilt.init(cards, {
+      max: 25,
+      speed: 400,
+      glare: true,
+      "max-glare": 0.5,
+    });
+
+    // Optional: bersihkan efek saat komponen di-unmount
+    return () => {
+      cards.forEach((card) => card.vanillaTilt?.destroy());
+    };
+  }, []);
+
     return(
         <section>
           <div className="skills">
@@ -10,22 +31,22 @@ function Sklills() {
                     </div>
                     </div>
                     <div className="row d-flex justify-content-evenly">
-                    <div className="col-3">
-                        <div className="skill-card text-center p-3">
-                        <img className="skill-img mt-3" src="src/assets/python.png" alt="" />
-                        <div className="skill-name my-3 fw-semibold">Python</div>
+                    <div className="col-3 ">
+                        <div className="skill-card text-center p-3" data-tilt>
+                            <img className="skill-img mt-3" src="src/assets/python.png" alt="" />
+                            <div className="skill-name my-3 fw-semibold">Python</div>
                         </div>
                     </div>
                     <div className="col-3">
-                        <div className="skill-card text-center p-3">
-                        <img className="skill-img mt-3" src="src/assets/atom.png" alt="" />
-                        <div className="skill-name my-3 fw-semibold">React</div>
+                        <div className="skill-card text-center p-3" data-tilt>
+                            <img className="skill-img mt-3" src="src/assets/atom.png" alt="" />
+                            <div className="skill-name my-3 fw-semibold">React</div>
                         </div>
                     </div>
                     <div className="col-3">
-                        <div className="skill-card text-center p-3">
-                        <img className="skill-img mt-3" src="src/assets/js.png" alt="" />
-                        <div className="skill-name my-3 fw-semibold">Javascript</div>
+                        <div className="skill-card text-center p-3" data-tilt>
+                            <img className="skill-img mt-3" src="src/assets/js.png" alt="" />
+                            <div className="skill-name my-3 fw-semibold">Javascript</div>
                         </div>
                     </div>
                     </div>
